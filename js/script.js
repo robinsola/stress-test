68fe14c584c0ecf98551d0227bf5a1e75b39c096
Robin Sola <rsolarms@gmail.com>
2017-11-30 16:27:06 -0800

    add html form for stress questions and button to submit. add script to collect data from user and display results.

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var stress = [];
+var methods = [];
+
+$(document).ready(function() {
+  $("form#stress-test").submit(function(event) {
+    event.preventDefault();
+
+    $("input:checkbox[name=stress-signs]:checked").each(function() {
+      var stressSigns = $(this).val();
+      stress.push(stressSigns);
+    });
+
+    $("input:checkbox[name=stress-symptoms]:checked").each(function() {
+      var stressSymptoms = $(this).val();
+      stress.push(stressSymptoms);
+    });
+
+    $("input:checkbox[name=stress-methods]:checked").each(function() {
+      var stressMethods = $(this).val();
+      methods.push(stressMethods);
+    });
+
+    if (stress.length > methods.length) {
+      $("#extreme-stress").show();
+    } else if (methods.length > stress.length) {
+      $("#no-stress").show();
+    } else if (methods.length = stress.length) {
+      $("#mild-stress").show();
+    }
+
+  });
+});
